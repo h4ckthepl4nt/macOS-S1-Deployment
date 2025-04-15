5d1dd5110ca217dbfd1c9c60fa44cd6812b57c9b
Brian Irish <61841580+h4ckthepl4nt@users.noreply.github.com>
2025-04-15 15:26:01 -0400

    S1Install.sh

---
+++ S1Install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+S1INSTALL=/Applications/SentinelOne/
+S1PKG=/Library/Application\ Support/AirWatch/Data/Munki/Managed\ Installs/Cache/Sentinel-Release-24-4-1-7830_macos_v24_4_1_7830-24.4.1.7830.pkg
+FILE=/Library/Application\ Support/AirWatch/Data/Munki/Managed\ Installs/Cache/Sentinel-Release-24-4-1-7830_macos_v24_4_1_7830-24.4.1.7830.pkg
+
+#Check if S1 is already installed
+if [ -d "$S1INSTALL" ]; then
+	echo 'S1 Already Installed'
+	exit 0
+fi
+
+#Check if the .pkg is available for install
+if [ ! -f "$S1PKG" ]; then
+	echo "S1 PKG Not yet available. Will try again in 4 hours."
+	exit 1
+else
+	echo "S1 PKG is available and will be copied in 5 seconds..."
+	sleep 5
+	echo "Installing S1 now..."
+
+	#Retreive generic user password from the keychain	(Recommend this method unless the password variable can be set another way)
+	password=$(security find-generic-password -a "USERNAME" -w)
+
+	#Copy payloads to local user directory
+	/bin/echo "eyJ1cmwiOiAiaHR0cHM6Ly91c2VhMS1jaHJpc3RpYW5hY2FyZS5zZW50aW5lbG9uZS5uZXQiLCAic2l0ZV9rZXkiOiAiZ19lZjUyZjRkYTRhMzQxNWUxIn0=" > "/Library/Application Support/AirWatch/Data/Munki/Managed Installs/Cache/com.sentinelone.registration-token"
+	echo $password | sudo -S cp '/Library/Application Support/AirWatch/Data/Munki/Managed Installs/Cache/com.sentinelone.registration-token' '/Users/USERNAME/Downloads'
+	echo $password | sudo -S cp '/Library/Application Support/AirWatch/Data/Munki/Managed Installs/Cache/Sentinel-Release-24-4-1-7830_macos_v24_4_1_7830-24.4.1.7830.pkg' '/Users/App.ws1.GCMac/Downloads/'
+
+	#Install S1 Agent
+	echo $password | sudo -S /usr/sbin/installer -pkg '/Users/USERNAME/Downloads/Sentinel-Release-24-4-1-7830_macos_v24_4_1_7830-24.4.1.7830.pkg' -target /
+
+	#Clean up
+	echo $password | sudo -S rm /Users/USERNAME/Downloads/com.sentinelone.registration-token
+	echo $password | sudo -S rm /Users/USERNAME/Downloads/Sentinel-Release-24-4-1-7830_macos_v24_4_1_7830-24.4.1.7830.pkg
+	echo "S1 is now installed"
+    exit 0
+fi
